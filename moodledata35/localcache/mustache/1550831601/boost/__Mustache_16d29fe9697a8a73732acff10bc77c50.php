7713d3013f627c5ac665585c673cf2993a1ab1e4
syedarshadali <arshad.syed483@gmail.com>
2019-02-25 14:52:42 +0530

    moodle35 config folder added

---
+++ moodledata35/localcache/mustache/1550831601/boost/__Mustache_16d29fe9697a8a73732acff10bc77c50.php
@@ -0,0 +1,50 @@
+<?php
+
+class __Mustache_16d29fe9697a8a73732acff10bc77c50 extends Mustache_Template
+{
+    private $lambdaHelper;
+
+    public function renderInternal(Mustache_Context $context, $indent = '')
+    {
+        $this->lambdaHelper = new Mustache_LambdaHelper($this->mustache, $context);
+        $buffer = '';
+
+        // 'events' section
+        $value = $context->find('events');
+        $buffer .= $this->section3d02d15d6c07927fca9e4d44426c6a01($context, $indent, $value);
+
+        return $buffer;
+    }
+
+    private function section3d02d15d6c07927fca9e4d44426c6a01(Mustache_Context $context, $indent, $value)
+    {
+        $buffer = '';
+    
+        if (!is_string($value) && is_callable($value)) {
+            $source = '
+    {{> block_myoverview/event-list-item }}
+';
+            $result = call_user_func($value, $source, $this->lambdaHelper);
+            if (strpos($result, '{{') === false) {
+                $buffer .= $result;
+            } else {
+                $buffer .= $this->mustache
+                    ->loadLambda((string) $result)
+                    ->renderInternal($context);
+            }
+        } elseif (!empty($value)) {
+            $values = $this->isIterable($value) ? $value : array($value);
+            foreach ($values as $value) {
+                $context->push($value);
+                
+                if ($partial = $this->mustache->loadPartial('block_myoverview/event-list-item')) {
+                    $buffer .= $partial->renderInternal($context, $indent . '    ');
+                }
+                $context->pop();
+            }
+        }
+    
+        return $buffer;
+    }
+
+}
---
+++ moodledata35/localcache/mustache/1550831601/boost/__Mustache_60904cf97837c12264ff58d882f7e32e.php
@@ -0,0 +1,152 @@
+<?php
+
+class __Mustache_60904cf97837c12264ff58d882f7e32e extends Mustache_Template
+{
+    private $lambdaHelper;
+
+    public function renderInternal(Mustache_Context $context, $indent = '')
+    {
+        $this->lambdaHelper = new Mustache_LambdaHelper($this->mustache, $context);
+        $buffer = '';
+
+        $buffer .= $indent . '<header id="page-header" class="row">
+';
+        $buffer .= $indent . '    <div class="col-12 pt-3 pb-3">
+';
+        $buffer .= $indent . '        <div class="card">
+';
+        $buffer .= $indent . '            <div class="card-body">
+';
+        $buffer .= $indent . '                <div class="d-flex">
+';
+        $buffer .= $indent . '                    <div class="mr-auto">
+';
+        $buffer .= $indent . '                    ';
+        $value = $this->resolveValue($context->find('contextheader'), $context);
+        $buffer .= $value;
+        $buffer .= '
+';
+        $buffer .= $indent . '                    </div>
+';
+        $buffer .= $indent . '
+';
+        // 'settingsmenu' section
+        $value = $context->find('settingsmenu');
+        $buffer .= $this->sectionE263ae1886b9f45bd4f372744ad770aa($context, $indent, $value);
+        $buffer .= $indent . '                </div>
+';
+        $buffer .= $indent . '                <div class="d-flex flex-wrap">
+';
+        // 'hasnavbar' section
+        $value = $context->find('hasnavbar');
+        $buffer .= $this->section3cf925dc793f08410de325fe96872767($context, $indent, $value);
+        $buffer .= $indent . '                    <div class="ml-auto d-flex">
+';
+        $buffer .= $indent . '                        ';
+        $value = $this->resolveValue($context->find('pageheadingbutton'), $context);
+        $buffer .= $value;
+        $buffer .= '
+';
+        $buffer .= $indent . '                    </div>
+';
+        $buffer .= $indent . '                    <div id="course-header">
+';
+        $buffer .= $indent . '                        ';
+        $value = $this->resolveValue($context->find('courseheader'), $context);
+        $buffer .= $value;
+        $buffer .= '
+';
+        $buffer .= $indent . '                    </div>
+';
+        $buffer .= $indent . '                </div>
+';
+        $buffer .= $indent . '            </div>
+';
+        $buffer .= $indent . '        </div>
+';
+        $buffer .= $indent . '    </div>
+';
+        $buffer .= $indent . '</header>';
+
+        return $buffer;
+    }
+
+    private function sectionE263ae1886b9f45bd4f372744ad770aa(Mustache_Context $context, $indent, $value)
+    {
+        $buffer = '';
+    
+        if (!is_string($value) && is_callable($value)) {
+            $source = '
+                    <div class="context-header-settings-menu">
+                        {{{settingsmenu}}}
+                    </div>
+                    ';
+            $result = call_user_func($value, $source, $this->lambdaHelper);
+            if (strpos($result, '{{') === false) {
+                $buffer .= $result;
+            } else {
+                $buffer .= $this->mustache
+                    ->loadLambda((string) $result)
+                    ->renderInternal($context);
+            }
+        } elseif (!empty($value)) {
+            $values = $this->isIterable($value) ? $value : array($value);
+            foreach ($values as $value) {
+                $context->push($value);
+                
+                $buffer .= $indent . '                    <div class="context-header-settings-menu">
+';
+                $buffer .= $indent . '                        ';
+                $value = $this->resolveValue($context->find('settingsmenu'), $context);
+                $buffer .= $value;
+                $buffer .= '
+';
+                $buffer .= $indent . '                    </div>
+';
+                $context->pop();
+            }
+        }
+    
+        return $buffer;
+    }
+
+    private function section3cf925dc793f08410de325fe96872767(Mustache_Context $context, $indent, $value)
+    {
+        $buffer = '';
+    
+        if (!is_string($value) && is_callable($value)) {
+            $source = '
+                    <div id="page-navbar">
+                        {{{navbar}}}
+                    </div>
+                    ';
+            $result = call_user_func($value, $source, $this->lambdaHelper);
+            if (strpos($result, '{{') === false) {
+                $buffer .= $result;
+            } else {
+                $buffer .= $this->mustache
+                    ->loadLambda((string) $result)
+                    ->renderInternal($context);
+            }
+        } elseif (!empty($value)) {
+            $values = $this->isIterable($value) ? $value : array($value);
+            foreach ($values as $value) {
+                $context->push($value);
+                
+                $buffer .= $indent . '                    <div id="page-navbar">
+';
+                $buffer .= $indent . '                        ';
+                $value = $this->resolveValue($context->find('navbar'), $context);
+                $buffer .= $value;
+                $buffer .= '
+';
+                $buffer .= $indent . '                    </div>
+';
+                $context->pop();
+            }
+        }
+    
+        return $buffer;
+    }
+
+}
